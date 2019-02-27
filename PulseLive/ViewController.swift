af152972e5569128a314af7c611888a9eb414818
Faris <faris.zaman@metricell.com>
2019-02-27 18:54:34 +0000

    Initial Commit

---
+++ PulseLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PulseLive
+//
+//  Created by Metricell Developer on 27/02/2019.
+//  Copyright © 2019 Metricell Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
